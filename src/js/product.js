263000b9be3e1f7210f9ff9747fcd793ffb4020b
Vermont Garcia <vermontgarcia@gmail.com>
2025-03-12 14:56:12 -0600

    Lint fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,12 +1,12 @@
-import { setLocalStorage } from './utils.mjs';
+import { getLocalStorage, setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
 
 const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
+  const cartItems = getLocalStorage('so-cart') || [];
   cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
+  setLocalStorage('so-cart', cartItems);
 }
 
 // add to cart button event handler
