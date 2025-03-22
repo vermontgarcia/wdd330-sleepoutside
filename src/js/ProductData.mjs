6aae054683396d14c36b0670bbe9fed93c7bd7f7
daniel3034 <danielh3034@gmail.com>
2025-03-22 18:26:17 -0500

    individual3

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,27 +1,27 @@
+const baseURL = import.meta.env.VITE_SERVER_URL;
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
   } else {
-    throw new Error('Bad Response');
+    throw new Error("Bad Response");
   }
 }
 
 export default class ProductData {
-  constructor(category) {
-    this.category = category;
-    this.path = `../json/${this.category}.json`;
+  async getData(category) {
+    try {
+      const response = await fetch(`${baseURL}products/search/${category}`);
+      const data = await convertToJson(response);
+      return data.Result;
+    } catch (error) {
+      console.error("Error fetching product data:", error);
+      return []; // Return an empty array if fetch fails
+    }
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data)
-      .catch ((error) => {
-        console.error("Error fetching product data:", error);
-        return []; // Return empty array if fetch fails
-      });
-  }
-  async findProductById(id) {
-    const products = await this.getData();
+
+  async findProductById(id, category) {
+    const products = await this.getData(category);
     return products.find((item) => item.Id === id);
   }
 }
--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ import { renderListWithTemplate } from "./utils.mjs";
 function productCardTemplate(product) {
   return `<li class="product-card">
     <a href="product_pages/index.html?product=${product.Id}">
-      <img src="${product.Image}" alt="Image of ${product.Name}" />
+      <img src="${product.ImageUrl}" alt="Image of ${product.Name}" />
     </a>
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
@@ -20,8 +20,8 @@ export default class ProductList {
   }
 
   async init() {
-    this.fullList = await this.dataSource.getData(); // Store all products
-    this.renderList(this.fullList); // Render all products initially
+    this.fullList = await this.dataSource.getData(this.category); // Pass category
+    this.renderList(this.fullList); // Render products
   }
 
   renderList(list) {
