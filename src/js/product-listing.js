bb256a7ce8681eb59a3c9c63127fe3ea350aaa48
vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
2025-03-28 14:56:21 -0600

    Promote DEV to MAIN (#28)
    * vite preview (#13)
    
    * week3 team activity (#14)
    
    W03 Team Activity: Dynamic Header and Footer
    
    * Dynamic Product List (#11)
    
    * - Remove Product data from main
    - Add ProductList class
    - Render Product list dynamically
    - Refactor code to utility functions
    
    * Lastest changes
    
    ---------
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    
    * individual3 (#15)
    
    * Dh  individual task (#19)
    
    * individual3
    
    * DH-individualTask
    
    * double quotes to single
    
    * npx eslint route --fix
    
    * Hot Fix
    
    Categories fixed
    
    Listing
    Categories
    FromParam
    Fixed ProductList
    Fixed Result
    Fixed Links
    
    * Hot Fix (#22) (#23)
    
    * vite preview (#13)
    
    * week3 team activity (#14)
    
    W03 Team Activity: Dynamic Header and Footer
    
    * Dynamic Product List (#11)
    
    * - Remove Product data from main
    - Add ProductList class
    - Render Product list dynamically
    - Refactor code to utility functions
    
    * Lastest changes
    
    ---------
    
    
    
    * individual3 (#15)
    
    * Dh  individual task (#19)
    
    * individual3
    
    * DH-individualTask
    
    * double quotes to single
    
    * npx eslint route --fix
    
    * Hot Fix
    
    Categories fixed
    
    Listing
    Categories
    FromParam
    Fixed ProductList
    Fixed Result
    Fixed Links
    
    ---------
    
    Co-authored-by: vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    
    * Main View and Product Listing View fixes (#24)
    
    * Hot Fix (#22)
    
    * vite preview (#13)
    
    * week3 team activity (#14)
    
    W03 Team Activity: Dynamic Header and Footer
    
    * Dynamic Product List (#11)
    
    * - Remove Product data from main
    - Add ProductList class
    - Render Product list dynamically
    - Refactor code to utility functions
    
    * Lastest changes
    
    ---------
    
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    
    * individual3 (#15)
    
    * Dh  individual task (#19)
    
    * individual3
    
    * DH-individualTask
    
    * double quotes to single
    
    * npx eslint route --fix
    
    * Hot Fix
    
    Categories fixed
    
    Listing
    Categories
    FromParam
    Fixed ProductList
    Fixed Result
    Fixed Links
    
    ---------
    
    Co-authored-by: vermontgarcia <40477541+vermontgarcia@users.noreply.github.com>
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    
    * Main View Fix
    - Refactor pearch product
    - Remove duplicated cards
    - Remove console logs
    - Update ProductList Class
    - Update utils renderListWithTemplate
    
    * Product Listig View Fix
    - Refactor pearch product
    - Remove duplicated cards
    - Remove console logs
    - Update ProductList Class
    - Update utils renderListWithTemplate
    
    * Product Card Template update with picture element for images
    
    * Product ID update in product listing index
    
    * Tents JSON update with Images property
    
    * Banner fix
    
    * Styles update
    
    * List and format
    
    * API integration
    
    * - Routes fixed
    - format
    - Cart logo svg file
    
    * - Products detail fix
    - get product by id API integration
    - Cart card update
    
    * - Product detal ref from cart
    - Fix for cart increment quantity
    
    ---------
    
    Co-authored-by: daniel3034 <95935178+daniel3034@users.noreply.github.com>
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    
    * fix add item to cart qty (#26)
    
    * Formatted the Search Bar and category (#27)
    
    Formated titles
    Category for the h2 and searchbar
    styling searchbar, and title
    Modyfing profuct-listing
    
    ---------
    
    Co-authored-by: daniel3034 <95935178+daniel3034@users.noreply.github.com>
    Co-authored-by: Vermont Garcia <vermontgarcia@gmail.com>
    Co-authored-by: daniel3034 <danielh3034@gmail.com>

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -5,6 +5,11 @@ import ProductList from './ProductList.mjs';
 const category = getParam('category'); // Get category from URL
 const productData = new ProductData(category);
 const productList = new ProductList(category, productData, qs('#product-list'));
+
+const formatedCategory = category.charAt(0).toUpperCase() + category.slice(1).toLowerCase();
+qs('#category').innerText = `${formatedCategory} Products`;
+qs('title').innerText = `${formatedCategory} | Product Listing`;
+
 productList.init();
 
 loadHeaderFooter();
